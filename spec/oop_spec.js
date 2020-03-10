2f0435f4f664dbf26eaf5ec37bf8bbdc07a0b55c
Thembela <mbelaselani@gmail.com>
2020-03-10 09:20:08 +0200

    first commit

---
+++ spec/oop_spec.js
@@ -0,0 +1,33 @@
+let myTest = require("../src/person");
+
+let person = new myTest.Person('Ryan', 30, 'male', ['being a hardarse', 'agile', 'ssd hard drives'])
+
+
+describe("constructor for the class 'Person'", () => {
+
+  it("should return name", function() {
+    expect(person.name).toEqual("Ryan");
+  });
+
+
+  it("should return age", function() {
+    expect(person.age).toEqual(30);
+  });
+
+
+  it("should return gender", function() {
+    expect(person.gender).toEqual("male");
+  });
+
+ 
+  it("should return interests", function() {
+    expect(person.interests).toEqual(['being a hardarse', 'agile', 'ssd hard drives']);
+  });
+
+  describe("function 'Hello' in a class 'Person'", function () {
+    
+      it("returns greeting", function () {
+        expect(person.Hello()).toEqual(`Hello, my name is Ryan and I am 30 years old. I'm a male, my interests are being a hardarse,agile,ssd hard drives`);
+      });
+  });
+});
